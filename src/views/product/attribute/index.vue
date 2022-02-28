8fa2f8d2463787300d8af8d4b6b84133ab4a1fbf
SweetLollipop <137623637@qq.com>
2022-02-28 22:58:20 +0800

    P144修改属性的查看与编辑模式切换

--- src/views/product/attribute/index.vue
+++ src/views/product/attribute/index.vue
@@ -206,6 +206,13 @@ export default {
       //由于数据结构当中存在对象里面套数组，数组里面又套对象，因此需要使用深拷贝解决
       //深拷贝，浅拷贝在面试的时候出现的频率很高，切记达到手写深拷贝与浅拷贝
       this.attributeInfo = cloneDeep(row);
+      //在修改某一个属性的时候，将相应的属性值元素添加上flag这个标记
+      this.attributeInfo.attrValueList.forEach((item) => {
+        //这样书写也可以给属性值添加flag字段，但是会发现视图不会跟着变化（因为flag不是响应式数据）
+        //因为 Vue 无法探测普通的新增 property,这样书写的属性并非响应式属性（数据变化师徒跟着变化）
+        //第一个参数：对象，第二个参数：添加新的响应式属性，第三个属性的属性值
+        this.$set(item, "flag", false);
+      });
     },
     //失去焦点的事件，切换为查看模式，展示span
     toLook(row) {
@@ -226,7 +233,7 @@ export default {
       if (isRepat) {
         this.$message.error("属性值不能重复!");
         return;
-      };
+      }
       //row: 形参是当前用户添加的最新的属性
       row.flag = false; //当前编辑模式变为查看模式（让input消失，显示span）
     },
